aca77bcc3e094ece982a1e9aac2bec4f975d7b11
Abemelek Mesfin <abemelekmesfin07@gmail.com>
2025-07-31 15:17:11 +0300

    commited

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,26 +1,27 @@
 import os
 import django
 
-# Setup Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relationship_app.settings') 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')  
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
-print("Books by George Orwell:")
+print(f"Books by {author_name}:")
 for book in books_by_author:
     print(f"- {book.title}")
 
 # List all books in a specific library
-library = Library.objects.get(name="Central Library")
-library_books = library.books.all()
-print(f"\nBooks in {library.name}:")
-for book in library_books:
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(f"\nBooks in {library_name}:")
+for book in books_in_library:
     print(f"- {book.title}")
 
 # Retrieve the librarian for a specific library
 librarian = Librarian.objects.get(library=library)
-print(f"\nLibrarian of {library.name}: {librarian.name}")
+print(f"\nLibrarian of {library_name}: {librarian.name}")
