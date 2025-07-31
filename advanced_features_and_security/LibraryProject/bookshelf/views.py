b9aa8433cc0f3d2d0f3e4ff499d94ac4fa5bb56f
Abemelek Mesfin <abemelekmesfin07@gmail.com>
2025-07-31 17:42:35 +0300

    commited

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -39,6 +39,15 @@ def delete_book(request, pk):
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
-
+@permission_required('bookshelf.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
 # This view safely handles user input by validating it through Django forms.
