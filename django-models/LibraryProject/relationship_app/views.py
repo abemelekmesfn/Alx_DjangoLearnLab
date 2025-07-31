c2732a239c63fefa00eaded16c2639aa13e4cd8c
Abemelek Mesfin <abemelekmesfin07@gmail.com>
2025-07-31 15:31:13 +0300

    commited

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
+from django.shortcuts import render
 from django.views.generic import DetailView
+from .models import Book, Library 
 
-# Function-based view to list all books
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
